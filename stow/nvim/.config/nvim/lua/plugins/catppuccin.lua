7c41e92a9c23940766a9510e45f0f7ed3c9cf911
shalevbell <shalevbell123@gmail.com>
2026-03-04 21:47:16 +0200

    dotfiles nvim

---
+++ stow/nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "Macchiato",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
---
+++ stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,57 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      -- This controls the sidebar explorer behavior
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+        files = {
+          args = {
+            "--type=d",
+            "--hidden",
+            "--ignore-case",
+          },
+        },
+        grep = {
+          args = {
+            "--column",
+            "--line-number",
+            "--no-heading",
+            "--color=never",
+            "--smart-case",
+            "--hidden",
+          },
+        },
+      },
+    },
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[
+_          _ _                            _     _
+| |__   ___| | | ___   __      _____  _ __| | __| |
+| '_ \ / _ \ | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |
+| | | |  __/ | | (_) |  \ V  V / (_) | |  | | (_| |
+|_| |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_|
+      ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "p", desc = "Projects", action = ':lua LazyVim.pick("projects")()' },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+  },
+}
